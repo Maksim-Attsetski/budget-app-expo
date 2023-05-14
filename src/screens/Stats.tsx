57c8a2e186b8024c976d8f46d41dafeed25e51ef
Maksim Attsetski <maks2001maks2001@gmail.com>
2023-05-14 15:12:38 +0300

    add test linkinng

--- src/screens/Stats.tsx
+++ src/screens/Stats.tsx
@@ -1,12 +1,23 @@
 import React, { FC, memo } from 'react';
-import { View } from 'react-native';
-import { Text } from '../UI';
+import { Share } from 'react-native';
+
+import { Button, Text } from '../UI';
 import { Layout } from '../widgets/App';
 
 const Stats: FC = () => {
+  const onShare = async () => {
+    const link =
+      'exp://u.expo.dev/a61b36bd-16b9-4006-8290-e61ab62f0f45?channel-name=preview&runtime-version=1.0.0';
+
+    await Share.share({
+      message: link,
+      url: link,
+    });
+  };
   return (
     <Layout>
       <Text>Stats</Text>
+      <Button onPress={onShare}>share</Button>
     </Layout>
   );
 };
--- src/widgets/App/Navigation.tsx
+++ src/widgets/App/Navigation.tsx
@@ -1,21 +1,27 @@
-import React, { FC, memo } from 'react';
-
+import React, { FC, memo, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Provider } from 'react-redux';
-
 import { screenList } from './types';
 import { useTheme } from '../../shared';
 import { reduxStore } from './state';
+
+import * as Linking from 'expo-linking';
+
+const prefix = Linking.createURL('/', { scheme: 'budgetapp' });
 
 const Stack = createNativeStackNavigator();
 
 const Navigation: FC = () => {
   const { color, backgroundColor } = useTheme();
 
+  const linking = {
+    prefixes: [prefix],
+  };
+
   return (
     <Provider store={reduxStore}>
-      <NavigationContainer>
+      <NavigationContainer linking={linking}>
         <Stack.Navigator screenOptions={{ statusBarColor: backgroundColor }}>
           {screenList.map(({ component, name, title, animation }) => (
             <Stack.Screen
