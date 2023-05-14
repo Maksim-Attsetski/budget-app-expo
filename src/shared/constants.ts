ed6c8b5d06695d40f2608fea1a2619647065adb7
Maksim Attsetski <maks2001maks2001@gmail.com>
2023-05-14 16:42:37 +0300

    change color

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,8 +1,8 @@
 export const colors = {
   dark: '#333',
   darkBlock: '#454545',
-  whiteBlock: '#f1f1f1',
-  white: '#fff',
+  white: '#f1f1f1',
+  whiteBlock: '#fff',
   transparent: 'transparent',
   purple: '#5460FE',
   green: '#3A984D',
