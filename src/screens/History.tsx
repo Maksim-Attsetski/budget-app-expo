38e32e1787cbe57c2735cb795c74678716323f5b
Maksim Attsetski <maks2001maks2001@gmail.com>
2023-08-12 17:10:18 +0300

    update history render

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,5 +1,5 @@
 import React, { FC, Fragment, memo, useEffect } from 'react';
-import { ScrollView } from 'react-native';
+import { FlatList, ScrollView } from 'react-native';
 
 import { colors, dateHelper } from '../shared';
 import { Card, Gap, Text } from '../UI';
@@ -7,7 +7,7 @@ import { Layout } from '../widgets/App';
 import { useBudget } from '../widgets/Budget';
 
 const History: FC = () => {
-  const { budget, setBudget } = useBudget();
+  const { budget, setBudget, budgetLoading } = useBudget();
 
   useEffect(() => {
     setBudget();
@@ -16,33 +16,59 @@ const History: FC = () => {
   return (
     <Layout>
       <Gap y={10} />
-      <ScrollView>
-        {budget
-          .filter((el) => el.uid)
-          .map((el) => (
-            <Fragment key={el.uid}>
-              <Card>
-                <Text
-                  style={{
-                    color: el.type === 'inc' ? colors.green : colors.purple,
-                    fontSize: 22,
-                  }}
-                >
-                  {el.type === 'inc' ? '+' : '-'} {el.value} р.
-                </Text>
-                {el.description && (
-                  <>
-                    <Gap y={7} />
-                    <Text>{el.description}</Text>
-                  </>
-                )}
-                <Gap y={7} />
-                <Text>{dateHelper.getBeautifulDate(el.date, '.')} </Text>
-              </Card>
+      {budgetLoading ? (
+        <FlatList
+          ListHeaderComponent={
+            <>
+              <Text
+                style={{ fontSize: 24, textAlign: 'center', fontWeight: '700' }}
+              >
+                Грузим историю...
+              </Text>
+              <Gap y={10} />
+            </>
+          }
+          scrollEnabled
+          ItemSeparatorComponent={() => <Gap y={7} />}
+          showsVerticalScrollIndicator={false}
+          refreshing
+          onRefresh={() => {}}
+          data={[1, 2, 3]}
+          renderItem={({ item }) => (
+            <Card style={{ maxHeight: 130 }} key={item} loading />
+          )}
+        />
+      ) : (
+        <FlatList
+          scrollEnabled
+          ItemSeparatorComponent={() => <Gap y={7} />}
+          showsVerticalScrollIndicator={false}
+          ListEmptyComponent={<Text>История пуста</Text>}
+          refreshing={budgetLoading}
+          onRefresh={setBudget}
+          data={budget}
+          renderItem={({ item }) => (
+            <Card key={item.uid}>
+              <Text
+                style={{
+                  color: item.type === 'inc' ? colors.green : colors.purple,
+                  fontSize: 22,
+                }}
+              >
+                {item.type === 'inc' ? '+' : '-'} {item.value} р.
+              </Text>
+              {item.description && (
+                <>
+                  <Gap y={7} />
+                  <Text>{item.description}</Text>
+                </>
+              )}
               <Gap y={7} />
-            </Fragment>
-          ))}
-      </ScrollView>
+              <Text>{dateHelper.getBeautifulDate(item.date, '.')} </Text>
+            </Card>
+          )}
+        />
+      )}
     </Layout>
   );
 };
--- src/widgets/Budget/useBudget.tsx
+++ src/widgets/Budget/useBudget.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { QueryFilterConstraint } from 'firebase/firestore';
+
 import { useActions, useTypedSelector } from '../../shared';
 import { useFirestore } from '../../shared';
 import { IBudget } from './types';
@@ -7,6 +9,7 @@ export const useBudget = () => {
   const { budget: data } = useTypedSelector((state) => state.budget);
   const { action } = useActions();
   const fbBudget = useFirestore('zefirka-budget');
+  const [loading, setLoading] = useState<boolean>(false);
 
   const budget = [...data].sort((a, b) => b.date - a.date);
 
@@ -14,29 +17,64 @@ export const useBudget = () => {
     whereArr: QueryFilterConstraint[] = [],
     limitVal: number = 10
   ): Promise<void> => {
-    const currentBudget = await fbBudget.getAll(whereArr, limitVal);
-    currentBudget && action.setBudgetAC(currentBudget.result);
+    try {
+      setLoading(true);
+      const currentBudget = await fbBudget.getAll(whereArr, limitVal);
+      currentBudget && action.setBudgetAC(currentBudget.result);
+    } catch (error) {
+      throw error;
+    } finally {
+      setLoading(false);
+    }
   };
 
   const onCreate = async (info: IBudget): Promise<void> => {
-    const newBudget = {
-      ...info,
-      date: Date.now(),
-    } as IBudget;
+    try {
+      setLoading(true);
+      const newBudget = {
+        ...info,
+        date: Date.now(),
+      } as IBudget;
 
-    const uid = await fbBudget.addWithId(newBudget);
-    action.budgetCreateAC({ ...newBudget, uid });
+      const uid = await fbBudget.addWithId(newBudget);
+      action.budgetCreateAC({ ...newBudget, uid });
+    } catch (error) {
+      throw error;
+    } finally {
+      setLoading(false);
+    }
   };
 
   const onUpdate = async (newBudget: IBudget): Promise<void> => {
-    await fbBudget.update(newBudget?.uid, newBudget);
-    action.budgetUpdateAC(newBudget);
+    try {
+      setLoading(true);
+      await fbBudget.update(newBudget?.uid, newBudget);
+      action.budgetUpdateAC(newBudget);
+    } catch (error) {
+      throw error;
+    } finally {
+      setLoading(false);
+    }
   };
 
   const onDelete = async (id: string): Promise<void> => {
-    await fbBudget.remove(id);
-    action.budgetDeleteAC(id);
+    try {
+      setLoading(true);
+      await fbBudget.remove(id);
+      action.budgetDeleteAC(id);
+    } catch (error) {
+      throw error;
+    } finally {
+      setLoading(false);
+    }
   };
 
-  return { budget, setBudget, onCreate, onUpdate, onDelete };
+  return {
+    budget,
+    budgetLoading: loading,
+    setBudget,
+    onCreate,
+    onUpdate,
+    onDelete,
+  };
 };
