2be2a2eb85ed5bfb5fdb5eda976553561181e563
Maksim Attsetski <maks2001maks2001@gmail.com>
2023-08-13 19:36:57 +0300

    update loading render on the home page

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,5 @@
-import { useMemo, memo, FC } from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { useMemo, memo, FC, useEffect } from 'react';
+import { FlatList, ScrollView, StyleSheet } from 'react-native';
 
 import { IScreen, colors } from '../shared';
 import { Layout } from '../widgets/App';
@@ -9,7 +9,7 @@ import { routes } from '../widgets/App/types';
 import { NearestOrder } from '../widgets/Orders';
 
 const HomeScreen: FC<IScreen> = ({ navigation }) => {
-  const { budget } = useBudget();
+  const { budget, setBudget, budgetLoading } = useBudget();
 
   const inc: number = budget.reduce(
     (acc, cur) => (cur.type === 'inc' ? acc + cur.value : acc),
@@ -47,47 +47,67 @@ const HomeScreen: FC<IScreen> = ({ navigation }) => {
     navigation.navigate(routes.history);
   };
 
+  useEffect(() => {
+    setBudget();
+  }, []);
+
   return (
     <Layout>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <Gap y={5} />
-        <Card>
-          <Flex justify='space-evenly'>
-            <Text style={styles.profitText}>Доход: {inc} р.</Text>
-            <Text style={styles.lossText}>Расходы: {dec} р.</Text>
-          </Flex>
-          <Gap y={10} />
-          <Flex justify='center'>
-            <Text style={styles.boldText}>Чистая прибыль: {inc - dec} р.</Text>
-          </Flex>
-        </Card>
-        <Gap y={7} />
-        <Card>
-          <ProgressChart data={chartData} />
-        </Card>
-        <Gap y={7} />
-        <Flex justify='space-between'>
-          <NearestOrder />
-          {budget[0] && (
-            <Card style={{ position: 'relative', width: '100%' }}>
-              <Button style={styles.layer} onPress={onPressActivity} />
-              <Title size='small' textAlign='left'>
-                Последняя активность
-              </Title>
-              <Gap y={5} />
-              <Title
-                size='small'
-                style={{
-                  color:
-                    budget[0].type === 'inc' ? colors.green : colors.purple,
-                }}
-              >
-                {budget[0].type === 'inc' ? '+' : '-'} {budget[0].value} р.
-              </Title>
+      <FlatList
+        data={[]}
+        renderItem={() => <></>}
+        showsVerticalScrollIndicator={false}
+        onRefresh={setBudget}
+        refreshing={budgetLoading}
+        ListHeaderComponent={
+          <>
+            <Gap y={5} />
+            <Card loading={budgetLoading} style={{ maxHeight: 130 }}>
+              <Flex justify='space-evenly'>
+                <Text style={styles.profitText}>Доход: {inc} р.</Text>
+                <Text style={styles.lossText}>Расходы: {dec} р.</Text>
+              </Flex>
+              <Gap y={10} />
+              <Flex justify='center'>
+                <Text style={styles.boldText}>
+                  Чистая прибыль: {inc - dec} р.
+                </Text>
+              </Flex>
             </Card>
-          )}
-        </Flex>
-      </ScrollView>
+            <Gap y={7} />
+            <Card
+              loading={budgetLoading}
+              style={{ maxHeight: budgetLoading ? 330 : '100%' }}
+              rows={3}
+              rowHeight={90}
+            >
+              <ProgressChart data={chartData} />
+            </Card>
+            <Gap y={7} />
+            <Flex justify='space-between'>
+              <NearestOrder />
+              {budget[0] && (
+                <Card style={{ position: 'relative', width: '100%' }}>
+                  <Button style={styles.layer} onPress={onPressActivity} />
+                  <Title size='small' textAlign='left'>
+                    Последняя активность
+                  </Title>
+                  <Gap y={5} />
+                  <Title
+                    size='small'
+                    style={{
+                      color:
+                        budget[0].type === 'inc' ? colors.green : colors.purple,
+                    }}
+                  >
+                    {budget[0].type === 'inc' ? '+' : '-'} {budget[0].value} р.
+                  </Title>
+                </Card>
+              )}
+            </Flex>
+          </>
+        }
+      />
     </Layout>
   );
 };
